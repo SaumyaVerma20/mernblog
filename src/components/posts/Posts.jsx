390c59e257f887cb3319e4b873ae60876cdd7ba4
SaumyaVerma20 <70267771+SaumyaVerma20@users.noreply.github.com>
2021-08-04 20:28:16 +0530

    map fun

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,6 +4,7 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
+      {posts.split(',')};
       {posts.map((p) => (
         <Post post={p} />
       ))}
